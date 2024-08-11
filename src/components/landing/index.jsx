a280435ba9d59a6ac97d167c826f67b89944183d
levaithnan <ladertrivendra@outlook.com>
2024-08-11 17:16:06 +0530

    up51581124

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -49,10 +49,17 @@ export default function Index() {
   })
 
   return (
-    <motion.main variants={slideUp} initial='initial' animate='enter' className={`${styles.main} ${neue_montreal.className}`}>
-      <Image 
+    <motion.main
+      variants={slideUp}
+      initial="initial"
+      animate="enter"
+      className={`${styles.main} ${neue_montreal.className}`}
+    >
+      <Image
         fill={true}
-        src={"/Images/snype_pic02.PNG"}
+        src={
+          "https://utfs.io/f/1062bd09-b3f4-468f-91a5-75ff81bf4485-6j6ybk.PNG"
+        }
         alt="background"
       />
       <div className={styles.sliderContainer}>
@@ -62,8 +69,17 @@ export default function Index() {
         </div>
       </div>
       <div data-scroll data-scroll-speed={0.1} className={styles.description}>
-        <svg width='9' height='9' viewBox='0 0 9 9' fill='none' xmlns='https://www.w3.org/2000/svg'>
-          <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
+        <svg
+          width="9"
+          height="9"
+          viewBox="0 0 9 9"
+          fill="none"
+          xmlns="https://www.w3.org/2000/svg"
+        >
+          <path
+            d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z"
+            fill="white"
+          />
         </svg>
         <p>Freelance</p>
         <p>Video Editor</p>
